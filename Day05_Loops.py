4158e5a7d572ac044baaba2213866a851ef043f1
imabhishekgupt <89855934+imabhishekgupt@users.noreply.github.com>
2021-09-23 13:53:52 +0530

    Create Day05_Loops.py

---
+++ Day05_Loops.py
@@ -0,0 +1,29 @@
+# Day 5: Loops
+
+# Task 
+# Given an integer, N, print its first 10 multiples. Each multiple N * i (where 1 <= i <= 10) should be printed on a new line in the form:
+# N x i = result.
+
+# Input Format
+# A single integer, N.
+
+# Constraints
+# 2 <= N <= 20
+
+# Output Format
+# Print 10 lines of output; each line i (where 1 <= i <= 10) contains the result of N * i in the form: 
+# N x i = result.
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+for i in range(1,11):
+    print(f'{n} x {i} = {n*i}')
